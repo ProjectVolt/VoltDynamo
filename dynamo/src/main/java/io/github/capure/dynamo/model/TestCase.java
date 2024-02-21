3736125ebc6d054d63aef81950ffc264cebd4f7e
Kacper Stolarek <stolarek83@gmail.com>
2024-02-21 14:39:23 +0100

    removed auto id generation

--- dynamo/src/main/java/io/github/capure/dynamo/model/TestCase.java
+++ dynamo/src/main/java/io/github/capure/dynamo/model/TestCase.java
@@ -2,7 +2,6 @@ package io.github.capure.dynamo.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,7 +15,6 @@ import lombok.Setter;
 @NoArgsConstructor
 public class TestCase {
     @Id
-    @GeneratedValue(strategy = jakarta.persistence.GenerationType.SEQUENCE)
     private Long id;
     @Column(nullable = false)
     private Long problemId;
