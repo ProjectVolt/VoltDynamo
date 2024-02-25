afd636f03f76ac186315207eec09ced124524d03
Kacper Stolarek <stolarek83@gmail.com>
2024-02-25 14:27:51 +0100

    added logger

--- dynamo/src/main/java/io/github/capure/dynamo/service/TestCaseService.java
+++ dynamo/src/main/java/io/github/capure/dynamo/service/TestCaseService.java
@@ -9,32 +9,40 @@ import io.github.capure.dynamo.exception.CannotSaveTestCaseException;
 import io.github.capure.dynamo.exception.TestCaseNotFoundException;
 import io.github.capure.dynamo.model.TestCase;
 import io.github.capure.dynamo.repository.TestCaseRepository;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 public class TestCaseService {
     @Autowired
     private TestCaseRepository testCaseRepository;
 
     public void deleteByIdAndProblemId(Long id, Long problemId) throws TestCaseNotFoundException {
         try {
+            log.info("Deleting test case with id {} and problem id {}", id, problemId);
             testCaseRepository.deleteByIdAndProblemId(id, problemId);
         } catch (Exception e) {
+            log.error("Test case not found. ProblemId: " + problemId + ", id: " + id, e);
             throw new TestCaseNotFoundException(problemId, id);
         }
     }
 
     public void addTestCase(TestCase testCase) throws CannotSaveTestCaseException {
         try {
+            log.info("Saving test case {}", testCase);
             testCaseRepository.save(testCase);
         } catch (Exception e) {
+            log.error("Cannot save test case. Test case: " + testCase, e);
             throw new CannotSaveTestCaseException(testCase);
         }
     }
 
     public List<TestCase> findAllByProblemId(Long problemId) throws TestCaseNotFoundException {
         try {
+            log.debug("Finding all test cases for problem id {}", problemId);
             return testCaseRepository.findByProblemId(problemId);
         } catch (Exception e) {
+            log.error("Test case not found. ProblemId: " + problemId, e);
             throw new TestCaseNotFoundException(problemId);
         }
     }
